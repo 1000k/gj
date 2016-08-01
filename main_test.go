3572783ee0cfa39ca13c8fee1013beeb1fb13ad0
SENDA Keijiro <senda.keijiro@gmail.com>
2016-08-01 16:53:54 +0900

    Refactoring

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import (
+	"os"
+	"testing"
+	// "github.com/1000k/gj"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func TestIndexHandler(t *testing.T) {
+	t.Skip("Not implemented yet")
+}
